072411a519ccb8ce92b9b2948d4e25af5e232c3c
ChoHyunGook <whgusrnr7755@gmail.com>
2023-03-14 17:37:43 +0900

    update 0.0.3

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http =require('http')
 const jwt = require('jsonwebtoken')
 const socketio = require('socket.io')
 const moment = require('moment-timezone')
-const ejs = require('ejs')
 
 
 
@@ -21,6 +20,48 @@ const STREAMING_NAME = process.env.STREAMING_NAME
 const EXPIRES_TIME =process.env.EXPIRES
 
 let count = 0;
+
+const opens = moment().tz('Asia/Seoul')
+const days1 = opens.format('dddd')
+const hours1 = opens.format('HH')
+let kodays1;
+let ampm1;
+let hour1;
+
+switch (days1){
+    case 'Sunday':
+        kodays1='일요일'
+        break;
+    case 'Monday':
+        kodays1='월요일'
+        break;
+    case 'Tuesday':
+        kodays1='화요일'
+        break;
+    case 'Wednesday':
+        kodays1='수요일'
+        break;
+    case 'Thursday':
+        kodays1='목요일'
+        break;
+    case 'Friday':
+        kodays1='금요일'
+        break;
+    case 'Saturday':
+        kodays1='토요일'
+        break;
+}
+
+if(hours1<=12){
+    ampm1 = '오전'
+    hour1 = hours1
+}else{
+    ampm1 = '오후'
+    hour1 = hours1-12
+}
+
+const today1 = opens.format(`YYYY년 MM월 DD일 ${kodays1} ${ampm1} ${hour1}:mm:ss`)
+
 
 app.get('/',(req,res)=>{
     count++;
@@ -65,10 +106,9 @@ app.get('/',(req,res)=>{
     }
 
     const today = m.format(`YYYY년 MM월 DD일 ${kodays} ${ampm} ${hour}:mm:ss`)
-    res.send(`@@@@@ ${today} 서버 켜져있음... @@@@@ 오픈 후 서버 접근 횟수 ${count}번 @@@@@ `)
+    res.send(`@@@@@ 서버오픈(재오픈) ${today1} @@@@@ ${today} 서버 켜져있음... @@@@@ 오픈 후 서버 접근 횟수 ${count}번 @@@@@ `)
 
 })
-
 
 
 
