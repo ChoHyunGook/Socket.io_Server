e4e6b02f9b564951d5ef6db28c4fe2f880f43442
ChoHyunGook <whgusrnr7755@gmail.com>
2023-03-17 17:03:01 +0900

    strings

--- app/service.js
+++ app/service.js
@@ -37,59 +37,60 @@ const service = function (){
                 const data = req.body
                 if(typeof data.MAC === 'string'|| typeof data.PORT === 'string'||
                     typeof data.MACPORT === 'string' || typeof data.IP === 'string'){
+                    res.status(400).send('String값임')
+                    // Info.findOne({MACPORT:req.body.MACPORT})
+                    //     .then((mb)=>{
+                    //         if(mb === null){
+                    //             const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+                    //             const connectDate = Date.connectDate()
+                    //
+                    //             const socketData = data.MACPORT
+                    //
+                    //             const ServerName = `/socket.io/${socketData}`
+                    //             const infoData = {
+                    //                 SERVERNAME: ServerName,
+                    //                 MAC: data.MAC,
+                    //                 IP: data.IP,
+                    //                 PORT: data.PORT,
+                    //                 MACPORT: socketData
+                    //             }
+                    //             const logDb = {log: `API::POST::${connectDate}::${ServerName}::${infoData.MAC}::${ip}::${logOpenDay}::/SocketServerCreate`}
+                    //
+                    //             const turnData = {
+                    //                 ip: ip,
+                    //                 SERVERNAME: ServerName,
+                    //                 MAC: data.MAC,
+                    //                 IP: data.IP,
+                    //                 PORT: data.PORT,
+                    //                 MACPORT: socketData
+                    //             }
+                    //
+                    //             new apiLogs(logDb).save()
+                    //                 .then(r => console.log('Log data Save...'))
+                    //                 .catch(err => console.log('Log Save Error', err))
+                    //
+                    //             new Info(infoData).save()
+                    //                 .then(r => console.log('Info Data Save...'))
+                    //                 .catch(err => console.log('Info Save Error', err))
+                    //
+                    //
+                    //             //소켓서버생성
+                    //             socket().socketService(turnData)
+                    //             console.log('Socket Server Creation Completed')
+                    //             res.status(200).json(ServerName);
+                    //
+                    //         }else {
+                    //             console.log('Socket Server Creation Fail...(MACPORT Duplication)')
+                    //             res.status(400).send('사용중인 MACPORT 주소입니다.')
+                    //         }
+                    //     })
+                    //     .catch(err=>{
+                    //         res.status(400).json(err)
+                    //     })
+                }else{
                     res.status(400).send('String값 필요')
+
                 }
-                Info.findOne({MACPORT:req.body.MACPORT})
-                    .then((mb)=>{
-                        if(mb === null){
-                            const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-                            const connectDate = Date.connectDate()
-
-                            const socketData = data.MACPORT
-
-                            const ServerName = `/socket.io/${socketData}`
-                            const infoData = {
-                                SERVERNAME: ServerName,
-                                MAC: data.MAC,
-                                IP: data.IP,
-                                PORT: data.PORT,
-                                MACPORT: socketData
-                            }
-                            const logDb = {log: `API::POST::${connectDate}::${ServerName}::${infoData.MAC}::${ip}::${logOpenDay}::/SocketServerCreate`}
-
-                            const turnData = {
-                                ip: ip,
-                                SERVERNAME: ServerName,
-                                MAC: data.MAC,
-                                IP: data.IP,
-                                PORT: data.PORT,
-                                MACPORT: socketData
-                            }
-
-                            new apiLogs(logDb).save()
-                                .then(r => console.log('Log data Save...'))
-                                .catch(err => console.log('Log Save Error', err))
-
-                            new Info(infoData).save()
-                                .then(r => console.log('Info Data Save...'))
-                                .catch(err => console.log('Info Save Error', err))
-
-
-                            //소켓서버생성
-                            socket().socketService(turnData)
-                            console.log('Socket Server Creation Completed')
-                            res.status(200).json(ServerName);
-
-                        }else {
-                            console.log('Socket Server Creation Fail...(MACPORT Duplication)')
-                            res.status(400).send('사용중인 MACPORT 주소입니다.')
-                        }
-                    })
-                    .catch(err=>{
-                        res.status(400).json(err)
-                })
-
-
 
             }catch (err){
                 res.status(400).json(err)
